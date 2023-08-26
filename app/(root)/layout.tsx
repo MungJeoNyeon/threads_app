75190d7e7afd13448cac84f72c2865f43529d69a
jounghyunMoon <moonjhyun0613@gmail.com>
2023-08-26 17:00:31 +0900

    errors fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,17 +5,18 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
-import Topbar from "@/components/shared/Topbar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'Treads',
-  description: 'A Next.js 13 Meta Treads Application'
-}
+export const metadata: Metadata = {
+  title: "Moonreads",
+  description: "A Next.js 13 Meta Threads application",
+  icons: "/logo.svg"
+};
 
 export default function RootLayout({
   children,
@@ -23,20 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang='en'>
         <body className={inter.className}>
           <Topbar />
 
-          <main className="flex flex-row">
+          <main className='flex flex-row'>
             <LeftSidebar />
-
-            <section className="main-container">
-              <div className="w-full max-w-4xl">
-                {children}
-              </div>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
-
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
 
